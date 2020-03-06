bbb0a2c00dfc4962bcc18a9015c8a9ad49bc1eb5
sheldon <sheldontaylor.7@gmail.com>
2020-03-06 14:57:09 -0400

    Inversion of Hash Function, Optimization and Refactor.

--- Driver.cpp
+++ Driver.cpp
@@ -1,5 +1,5 @@
 /* 
- * driver.cpp
+ * Driver.cpp
  * Sheldon Taylor
  * 
  * Based on Chris Little's implementation of Perlin Noise (used primarily for SDL2 implementation). 
@@ -12,28 +12,113 @@
 
 using namespace std;
 
+#define TESTING 0	// Testing mode off
+//#define TESTING 1	// Testing mode on
+
+#define WIDTH 1000
+#define HEIGHT 750
+
+// Define Pairing Function to be used (uncomment selected PAIRING_FUNCTION)
+//#define PAIRING_FUNCTION 0 	// Linear
+#define PAIRING_FUNCTION 1 	// Cantor
+//#define PAIRING_FUNCTION 2 	// Szudzik
+
+
+int linearPair(int x, int y) {
+	return (y * WIDTH + x);
+}
+
+// Cantor Pairing Function
+int cantorPair(int x, int y) {
+	return (((x + y) * (x + y + 1)) / 2 + y);
+}
+
+// Cantor Inversion Helper (Q value calculation)
+int cantorHelper(int z) {
+	return (floor((-1 + sqrt(1 + 8 * z)) / 2));
+}
+
+// Cantor Inversion of X value
+int cantorInvertX(int z) {
+	int q = cantorHelper(z);
+	return ((q * (3 + q)) / 2 - z);
+}
+
+// Cantor Inversion of Y value
+int cantorInvertY(int z) {
+	int q = cantorHelper(z);
+	return (z - (q * (1 + q)) / 2);
+}
+
+// Szudzik Pairing Function
+int szudzikPair(int x, int y) {
+	return ((x >= y) ? (pow(x, 2) + x + y) : (pow(y, 2) + x));	
+}
+
+// Szudzik Inversion of X value
+int szudzikInvertX(int z) {
+	int temp = floor(sqrt(z));
+	if (z - pow(temp, 2) < temp) {
+		return (z - pow(temp, 2));
+	} else {
+		return temp;
+	}
+
+}
+
+// Szudzik Inversion of Y value
+int szudzikInvertY(int z) {
+	int temp = floor(sqrt(z));
+	if (z - pow(temp, 2) < temp) {
+		return temp;
+	} else {
+		return (z - pow(temp, 2) - temp);
+	}
+}
+
+
+#if PAIRING_FUNCTION == 0	// Linear
+	#define TITLE 		"Perlin Noise - Linear" 
+	#define HASH(x, y)  	(y * WIDTH + x)
+#elif PAIRING_FUNCTION == 1	// Cantor
+	#define TITLE 		"Perlin Noise - Cantor"
+	#define HASH(x, y)  	cantorPair(x, y)
+	#define INVERT_X(z)	cantorInvertX(z)
+	#define INVERT_Y(z)	cantorInvertY(z)
+#elif PAIRING_FUNCTION == 2	// Szudzik
+	#define TITLE 		"Perlin Noise - Szudzik"
+	#define HASH(x, y)  	szudzikPair(x, y)
+	#define INVERT_X(z)	szudzikInvertX(z)
+	#define INVERT_Y(z)	szudzikInvertY(z)
+#else				// Other
+	#define TITLE 		"Perlin Noise - Other"
+	#define HASH(x, y)  	(y * WIDTH + x)
+#endif
+
 // Colours pixel at given position, (x, y). Assumes 32 bits per pixel.
 void colourPixel(SDL_Surface* surface, int x, int y, Uint32 pixel) {
 	Uint32 *p = (Uint32 *)surface -> pixels + y * surface -> w + x;
 	*p = pixel;
 }
 
+
 #undef main
 
+
 int main() {
-	//int width = 1000;
-	int width = 400;
-	//int height = 750;
-	int height = 300;
+
+	// Define array size
+	unsigned long long int arr_size = pow(WIDTH, 2) * pow(HEIGHT, 2);
+	int *indexArray = new int[WIDTH * HEIGHT];
+	int indexArrayCurr = 0;
 
 	// Initialize noise
 	Fractal *noiseGenerator = new Fractal();
 	noiseGenerator -> setInitFrequency(4.0f);
 
-	// Initialize data structure to store noise
-	//int arraySize = width * height;  // Linear (N)
-	int arraySize = width * width * height * height;  // Cantor Pairing (NxN)	
-	float *noiseArray = new float[arraySize];
+	// Intialize nosie array 	
+	//float *noiseArray = new float[ARRAY_SIZE];
+	float *noiseArray = new float[arr_size];
 
 	// Initialize SDL window, renderer, image and texture
 	SDL_Window *window = NULL;
@@ -46,8 +131,8 @@ int main() {
 		cout << "SDL couldn't init." << endl;
 		return 1;
 	}
-
-	window = SDL_CreateWindow("Perlin Noise", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
+	
+	window = SDL_CreateWindow(TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);	
 	if (!window) {
 		cout << "SDL couldn't create window." << endl;
 		return 2;
@@ -58,7 +143,6 @@ int main() {
 		cout << "SDL couldn't retrieve the renderer." << endl;
 		return 3;
 	}
-
 
 	// Create an SDL surface to write the noise values to.
 	Uint32 rmask, gmask, bmask, amask;
@@ -74,42 +158,32 @@ int main() {
 	amask = 0xff000000;
 #endif
 
-	image = SDL_CreateRGBSurface(SDL_SWSURFACE, width, height, 32, rmask, gmask, bmask, amask);
+	image = SDL_CreateRGBSurface(SDL_SWSURFACE, WIDTH, HEIGHT, 32, rmask, gmask, bmask, amask);
 	if (!image) {
 		cout << "Failed to create surface." << endl;
 		return 4;
 	}
 
 	// Generate a noise value for each pixel
-	float invWidth = 1.0f / float(width);
-	float invHeight = 1.0f / float(height);
+	float invWidth = 1.0f / float(WIDTH);
+	float invHeight = 1.0f / float(HEIGHT);
 	float noise;
 	float min = 0.0f;
 	float max = 0.0f;
-	//int index;
-	//vector<int> indexVec;
-	int *indexSetArray = new int[arraySize];
-	int *indexGetArray = new int[arraySize];
+	
 	Uint8 colourByte;
 
-	for (int x = 0; x < width; ++x) {
-		for (int y = 0; y < height; ++y) {
+	for (int x = 0; x < WIDTH; ++x) {
+		for (int y = 0; y < HEIGHT; ++y) {
 
+			// Generate noise value 
 			noise = noiseGenerator -> noise(float(x) * invWidth, float(y) * invHeight, 0.72);
 	
-			// Initial Hash Function
-			//int index = y * width + x;
-
-			// Cantor Pairing Function
-			//int index = ((x + y) * (x + y + 1)) / 2 + y;
-
-			// TO DO: IMPLEMENT SZUDZIK PAIRING FUNCITON
-			// Szudzik Pairing Function
-			int index = (x > y) ? (x*x + x + y) : (y*y + x);
-			
+			// Set noise value dependant on hashed value
+			int index = HASH(x, y);
+			indexArray[indexArrayCurr++] = index;
 			noiseArray[index] = noise;
-			indexSetArray[index] = index;
-	
+			
 			// Keep track of minimum and maximum noise values
 			if (noise < min) {
 				min = noise;
@@ -124,53 +198,42 @@ int main() {
 	// Convert noise values to pixel colour values.
 	float temp = 1.0f / (max - min);
 
-	// TO DO: IMPLEMENT INVERSION OF CANTOR/SZUDZIK PAIRING FUNCITONS
-	//
-	//
-
-	for (int x = 0; x < width; ++x) {
-		for (int y = 0; y < height; ++y) {
-
-			// Use gaussian distribution of noise values to fill [-1, 1] range.
-			
-			// Initial Hash Function
-			//int index = y * width + x;
-
-			// Cantor Pairing Function
-			//int index = ((x + y) * (x + y + 1)) / 2 + y;
-
-			// Szudzik Pairing Function
-			int index = (x > y) ? (x*x + x + y) : (y*y + x);
-			
-			//cout << index << endl;
-			noise = noiseArray[index];
-			indexGetArray[index] = index;
-
-			noise = -1.0f + 2.0f * (noise - min) * temp;
+	// Invert Hash Functions	
+	for (int i = 0; i < (HEIGHT * WIDTH); i++) {
 		
-			// Remap to RGB friendly colour values in range [0, 1].
-			noise += 1.0f;
-			noise *= 0.5f;
+		int index = indexArray[i];	
+		int inv_x = INVERT_X(index);
+		int inv_y = INVERT_Y(index);
 
-			colourByte = Uint8(noise * 0xff);
-
-			colourPixel(image, x, y, SDL_MapRGB(image -> format, colourByte, colourByte, colourByte));
-		}
-	}
-
-	// FOR TESTING PURPOSES ONLY
-	for (int x = 0; x < width; ++x) {
-		for (int y = 0; y < height; ++y) {
+		noise = noiseArray[index];
 			
-			//int index = y * width + x; // Initial function (linear)
-			//int index = ((x + y) * (x + y + 1)) / 2 + y; // Cantor Pairing function
-			int index = (x > y) ? (x*x + x + y) : (y*y + x); // Szudzik Pairing function
+		// Use gaussian distribution of noise values to fill [-1, 1] range.
+		noise = -1.0f + 2.0f * (noise - min) * temp;
+		
+		// Remap to RGB friendly colour values in range [0, 1].
+		noise += 1.0f;
+		noise *= 0.5f;
+		
+		colourByte = Uint8(noise * 0xff);
+		colourPixel(image, inv_x, inv_y, SDL_MapRGB(image -> format, colourByte, colourByte, colourByte));
+	}
+	
+	// FOR TESTING PURPOSES
+	if (TESTING) {
+		for (int x = 0; x < WIDTH; ++x) {
+			for (int y = 0; y < HEIGHT; ++y) {
+				// Invert Hash Function
+				int index = HASH(x, y);
 
-			cout << "x: " << x << " y: " << y << " | indexSet: " << indexSetArray[index] 
-				<< " | indexGet: " << indexGetArray[index] << endl;
+				int inv_x = INVERT_X(index);
+				int inv_y = INVERT_Y(index);
+			
+				printf("Hashed Index: %d\n", index);
+				printf("Actual X: %d  |  Inverted X: %d\n", x, inv_x);
+				printf("Actual Y: %d  |  Inverted Y: %d\n\n", y, inv_y);
+			}
 		}
 	}
-
 
 	// Convert surface to texture that can be copied to the window.
 	imageTexture = SDL_CreateTextureFromSurface(renderer, image);
@@ -179,6 +242,9 @@ int main() {
 		return 5;
 	}
 
+	// TO DO: IMPLEMENT WRITE TO BMP/PNG
+	//SDL_SaveBMP(image, "out.bmp");
+	
 	SDL_FreeSurface(image);
 
 	// Copy image to frame buffer and display
@@ -214,10 +280,8 @@ int main() {
 	SDL_Quit();
 
 	delete[] noiseArray;
-	delete[] indexSetArray;
-	delete[] indexGetArray;
 	delete noiseGenerator;
-
+	
 	return 0;
 }
 
